152a9638338e2ace4b0108fa1e8abcfdf45b40cc
48013471 <241117@student.pwr.edu.pl>
2020-01-05 23:59:46 +0100

    Problem with get_all_advertisements.

--- controllers/general.js
+++ controllers/general.js
@@ -1,16 +1,16 @@
 //instance of database connection
 var db = require('./db_connection');
-
-var qr1 = function(query){
-    var array = [];
+var temp_array = [];
+var qr1 = (query) => {
+    //let array = [];
     db.instance.query(query, (err,rows,fields) => {
         
          for(let row of rows){
             // console.log(row);
-             array.push({id : row.idZwierze, name : row.imie, Spieces : row.gatunek, age : row.wiek, sex : row.plec});
-             console.log(array);
+             temp_array.push({id : row.idZwierze, name : row.imie, Spieces : row.gatunek, age : row.wiek, sex : row.plec});
+             //console.log('1');
          };
-         console.log(`PRZED RETURNEM: ${array}`);
+        // console.log(`PRZED RETURNEM: ${array}`);
          
     })
     console.log(`TU JUŻ NIE WIDZI: ${array}`)
@@ -20,23 +20,18 @@ var qr1 = function(query){
 
 
 module.exports.get_all_advertisements = (req,res) => {
-    //console.log(`ccasca ${qr1(`SELECT Opieka.idOpieka, Zwierze.idZwierze, Zwierze.imie, Zwierze.plec, Zwierze.wiek, Zwierze.gatunek FROM Opieka
-//JOIN Zwierze ON Opieka.Zwierze_idZwierze = Zwierze.idZwierze WHERE Opieka.Ogloszenie_idOgloszenie = 9`)}`)
-    
-    
-    const general_rows = [];
 
     let query = `SELECT * FROM Ogloszenie JOIN Lokalizacja ON Lokalizacja_idLokalizacja = idLokalizacja`;
     
-
-
     db.instance.query(query, (err, rows, fields) => {
 
-        
+        const Schema = {
+            object : []
+        }
 
         for(let row of rows) {
             
-            const schema = {
+            Schema.object.push( {
                 id : row.idOgloszenie,
                 title : null,
                 contents : row.tresc,
@@ -50,21 +45,33 @@ module.exports.get_all_advertisements = (req,res) => {
                 },
                 care : {
                     id : null,
-                    animals : qr1(`SELECT Opieka.idOpieka, Zwierze.idZwierze, Zwierze.imie, Zwierze.plec, Zwierze.wiek, Zwierze.gatunek FROM Opieka
-                    JOIN Zwierze ON Opieka.Zwierze_idZwierze = Zwierze.idZwierze WHERE Opieka.Ogloszenie_idOgloszenie = ${row.idOgloszenie}`)
+                    animals : undefined
                 }
                 
-            }
-           
-            console.log(`ZWRÓCONA WARTOŚĆ ${qr1(`SELECT Opieka.idOpieka, Zwierze.idZwierze, Zwierze.imie, Zwierze.plec, Zwierze.wiek, Zwierze.gatunek FROM Opieka
-            JOIN Zwierze ON Opieka.Zwierze_idZwierze = Zwierze.idZwierze WHERE Opieka.Ogloszenie_idOgloszenie = ${row.idOgloszenie}`)}`);
+            })
 
-            general_rows.push(schema);
-            //console.log(schema);
-            
+            db.instance.query(`SELECT Opieka.idOpieka, Zwierze.idZwierze, Zwierze.imie, Zwierze.plec, Zwierze.wiek, Zwierze.gatunek FROM Opieka 
+            JOIN Zwierze ON Opieka.Zwierze_idZwierze = Zwierze.idZwierze WHERE Opieka.Ogloszenie_idOgloszenie = ${row.idOgloszenie}`, (err, rows, fields) => {
+                
+                for(let o in Schema.object){
+                    if (o.id === row.idOgloszenie) {
+                        o.animals = rows;
+                    }
+                }
+                res.send(Schema);
+                
+
+            })
+
         };
+
+        
+
+
+
+        
     })
     
-    res.send(general_rows);
+   
     //console.log(general_rows);
 }
